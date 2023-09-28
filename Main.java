5a786017b5dcbf179ff99b339611228517f21889
Juank1217 <139294435+Juank1217@users.noreply.github.com>
2023-09-28 13:08:14 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,22 @@
+
+//import Vista.MainGUI;
+import Controlador.ControladorJuego;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author ander
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ControladorJuego.iniciar();
+    }
+    
+}
